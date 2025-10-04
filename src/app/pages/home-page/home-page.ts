00927b9e6860b259d836801702bd029f78c0509a
Myroslava <milrusysurlim@gmail.com>
2025-10-04 15:11:46 +0300

    implement home-page

---
+++ src/app/pages/home-page/home-page.ts
@@ -0,0 +1,42 @@
+import { Component, signal } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RandomCountriesWidget } from "../../components/random-countries-widget/random-countries-widget";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HttpClientModule, NgFor, FormsModule, RandomCountriesWidget],
+  templateUrl: './home-page.html',
+  styleUrls: ['./home-page.scss']
+})
+export class Home {
+  countries = signal<any[]>([]);
+  searchTerm = signal('');
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  goToCountry(country: {countryCode: string; name: string }) {
+    this.router.navigate(['/country', country.countryCode, country.name]);
+  }
+
+  fetchCountries() {
+    this.http.get<any[]>('https://date.nager.at/api/v3/AvailableCountries')
+      .subscribe({
+        next: data => {
+          const term = this.searchTerm();
+          if (term) {
+            const filtered = data.filter(c =>
+              c.name.toLowerCase().includes(term.toLowerCase())
+            );
+            this.countries.set(filtered);
+          } else {
+            this.countries.set(data);
+          }
+        },
+        error: err => console.error(err)
+      });
+  }
+}
